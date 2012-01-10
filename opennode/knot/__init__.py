aaf58087c1a14cae94f90db303998a350f8e9feb
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-01-10 15:46:21 +0100

    Added support for knot specific configuration files

--- opennode/knot/__init__.py
+++ opennode/knot/__init__.py
@@ -1,6 +1,16 @@
-from grokcore.component import implements
+from grokcore.component import implements, Subscription, context
 
+import opennode.knot
+from opennode.oms.config import IRequiredConfigurationFiles, gen_config_file_names
 from opennode.oms.model.model.plugins import IPlugin, PluginInfo
+
+
+class KnotRequiredConfigurationFiles(Subscription):
+    implements(IRequiredConfigurationFiles)
+    context(object)
+
+    def config_file_names(self):
+        return gen_config_file_names(opennode.knot, 'knot')
 
 
 class KnotPlugin(PluginInfo):
