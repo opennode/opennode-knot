f7ac6ef9020f99f0519bae57d4f7be6d4866541c
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-01-12 02:35:35 +0100

    Added back knot specific creatables models

--- opennode/knot/__init__.py
+++ opennode/knot/__init__.py
@@ -1,8 +1,18 @@
 from grokcore.component import implements, Subscription, context
 
 import opennode.knot
+
 from opennode.oms.config import IRequiredConfigurationFiles, gen_config_file_names
+from opennode.oms.model.model import creatable_models
 from opennode.oms.model.model.plugins import IPlugin, PluginInfo
+
+from opennode.knot.model.compute import Compute
+from opennode.knot.model.template import Template
+from opennode.knot.model.virtualizationcontainer import VirtualizationContainer
+from opennode.knot.model.hangar import Hangar
+from opennode.knot.model.storage import Storage
+from opennode.knot.model.network import Network, NetworkInterface
+from opennode.knot.model.console import VncConsole
 
 
 class KnotRequiredConfigurationFiles(Subscription):
@@ -19,3 +29,7 @@ class KnotPlugin(PluginInfo):
     def initialize(self):
         print "[KnotPlugin] initializing plugin"
 
+        compute_creatable_models = dict((cls.__name__.lower(), cls)
+                                        for cls in [Compute, Template, Network, NetworkInterface, Storage, VirtualizationContainer, Hangar, VncConsole])
+
+        creatable_models.update(compute_creatable_models)
