opennode/opennode-knot

commit be420fff4580c7ac74a9410c0e17a6d397c94c2a

Fixed transaction issue in sync, closes #146

opennode/knot/backend/func/compute.py
@@ -88,16 +88,12 @@ class SyncAction(Action):
 
     action('sync')
 
-    @db.transact
-    def execute(self, cmd, args):
-        blocking_yield(self._execute(cmd, args))
-
     @defer.inlineCallbacks
-    def _execute(self, cmd, args):
-        default = self.default_console()
+    def execute(self, cmd, args):
+        default = yield self.default_console()
 
         try:
-            self.sync_consoles()
+            yield self.sync_consoles()
             yield self.sync_hw()
 
             if IFuncInstalled.providedBy(self.context):
@@ -114,8 +110,13 @@ class SyncAction(Action):
         except Exception as e:
             cmd.write("%s\n" % (": ".join(msg for msg in e.args if isinstance(msg, str) and not msg.startswith('  File "/'))))
 
-    @db.assert_transact
+
+    @db.transact
     def default_console(self):
+        return self._default_console
+
+    @db.assert_transact
+    def _default_console(self):
         default = self.context.consoles['default']
         if default:
             return default.target.__name__
@@ -133,8 +134,12 @@ class SyncAction(Action):
                 default = 'ssh'
         self.context.consoles.add(Symlink('default', self.context.consoles[default]))
 
-    @db.assert_transact
+    @db.transact
     def sync_consoles(self):
+        return self._sync_consoles()
+
+    @db.assert_transact
+    def _sync_consoles(self):
         self.context.consoles = Consoles()
         address = self.context.hostname
         try:

opennode/knot/backend/func/virtualizationcontainer.py
@@ -144,8 +144,8 @@ class SyncVmsAction(Action):
             remote_vm = matching[0]
 
             # todo delegate all this into the action itself
-            default_console = action.default_console()
-            action.sync_consoles()
+            default_console = action._default_console()
+            action._sync_consoles()
             action.sync_vm(remote_vm)
             action.create_default_console(default_console)
 

commit e18d6a3c389f1bb8819da101acb624f4db855dce

Fixed bug introduced in be420fff, which caused re-creation of default console symlink

opennode/knot/backend/func/compute.py
@@ -113,7 +113,7 @@ class SyncAction(Action):
 
     @db.transact
     def default_console(self):
-        return self._default_console
+        return self._default_console()
 
     @db.assert_transact
     def _default_console(self):