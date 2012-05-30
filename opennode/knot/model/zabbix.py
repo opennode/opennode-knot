f6529b9b77d1b887a6416ee1a8535961616a7a63
Ilja Livenson <ilja.livenson@gmail.com>
2012-05-30 09:24:02 +0300

    Fixed an incorrect zope.schema field type

--- opennode/knot/model/zabbix.py
+++ opennode/knot/model/zabbix.py
@@ -11,7 +11,7 @@ class IZabbixConfiguration(Interface):
     zabbix_dns_name = schema.TextLine(title=u"Zabbix agent's DNS name", min_length=1, required=False)
     zabbix_ipv4_address = schema.TextLine(title=u"Zabbix agent's IPv4 address", min_length=7, required=False)
 
-    zabixx_agent_port = schema.Bool(title=u"Zabbix agent port", description=u"Port where zabbix agent is listening",
+    zabixx_agent_port = schema.Int(title=u"Zabbix agent port", description=u"Port where zabbix agent is listening",
         required=False, min=1, max=65535)
 
     zabbix_use_dns = schema.Bool(title=u'Use DNS',
