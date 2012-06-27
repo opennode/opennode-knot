opennode/opennode-knot

commit a42eb336bf6025f0397d0a9e44a9d7efd769939b

Fixed VM console hostname ip update (ON-437)

opennode/knot/backend/func/compute.py
@@ -145,15 +145,12 @@ class SyncAction(Action):
 
     @db.transact
     def sync_consoles(self):
-        if self.context['consoles'] and self.context.consoles['ssh']:
-            return self.fixup_console_ip(self.context.consoles['ssh'])
-
         return self._sync_consoles()
 
     @db.assert_transact
     def _sync_consoles(self):
-        if self.context.consoles['ssh']:
-            return
+        if self.context['consoles'] and self.context.consoles['ssh']:
+            return self.fixup_console_ip(self.context.consoles['ssh'])
 
         address = self.context.hostname
         try:

commit 81d92d7d621dd37641f032c5130348a2ea9eadb4

Fixes VM sync issue caused by commit a42eb336bf6, in turn caused by excessive ZODB commit suppression optimization (ON-444)

opennode/knot/backend/func/compute.py
@@ -166,8 +166,6 @@ class SyncAction(Action):
             address = self.context.ipv4_address.split('/')[0]
             if console.hostname != address:
                 console.hostname = address
-            else:
-                raise db.RollbackException()
 
     @defer.inlineCallbacks
     def _sync_virtual(self):