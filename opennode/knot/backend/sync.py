f924c63d7372e5efb4687c13768fdc07bc72fa6b
Ilja Livenson <ilja.livenson@gmail.com>
2012-08-01 13:35:23 +0300

    Implemented PingCheck action and daemon for verifying state of the Computes (ON-586)

--- opennode/knot/backend/sync.py
+++ opennode/knot/backend/sync.py
@@ -1,19 +1,105 @@
 import traceback
 
-from certmaster import certmaster
 from twisted.internet import defer
 from uuid import uuid5, NAMESPACE_DNS
+
+from certmaster import certmaster
+
 from zope.component import provideSubscriptionAdapter
 from zope.interface import implements
+from grokcore.component.directive import context
 
-from opennode.oms.config import get_config
-from opennode.oms.model.model.proc import IProcess, Proc, DaemonProcess
 from opennode.knot.model.compute import ICompute
-from opennode.oms.util import subscription_factory, async_sleep
+from opennode.knot.model.compute import Compute
+from opennode.knot.utils.icmp import ping
+
+from opennode.oms.model.model.actions import Action, action
+from opennode.oms.model.model.proc import IProcess, Proc, DaemonProcess
+from opennode.oms.config import get_config
+from opennode.oms.util import subscription_factory, async_sleep, blocking_yield
 from opennode.oms.zodb import db
 from opennode.oms.model.model.symlink import follow_symlinks
-from opennode.knot.model.compute import Compute
 from opennode.oms.endpoint.ssh.detached import DetachedProtocol
+
+
+class PingCheckAction(Action):
+    """Remove request of the host for joining OMS/certmaster"""
+    context(ICompute)
+
+    action('ping-check')
+
+    @defer.inlineCallbacks
+    def execute(self, cmd, args):
+        yield self._execute(cmd, args)
+
+    @db.transact
+    def _execute(self, cmd, args):
+        address = self.context.hostname.encode('utf-8')
+        res = ping(address)
+        self.context.last_ping = (res == 1)
+
+
+class PingCheckDaemonProcess(DaemonProcess):
+    implements(IProcess)
+
+    __name__ = "ping-check"
+
+    def __init__(self):
+        super(PingCheckDaemonProcess, self).__init__()
+
+        config = get_config()
+        self.interval = config.getint('pingcheck', 'interval')
+
+    @defer.inlineCallbacks
+    def run(self):
+        while True:
+            try:
+                if not self.paused:
+                    yield self.ping_check()
+            except Exception:
+                if get_config().getboolean('debug', 'print_exceptions'):
+                    traceback.print_exc()
+
+            yield async_sleep(self.interval)
+
+    def log(self, msg):
+        import threading
+        print "[ping-check] (%s) %s" % (threading.current_thread(), msg)
+
+    @defer.inlineCallbacks
+    def ping_check(self):
+
+        @db.ro_transact
+        def get_computes():
+            res = []
+
+            oms_root = db.get_root()['oms_root']
+            for i in [follow_symlinks(i) for i in oms_root['computes'].listcontent()]:
+                if ICompute.providedBy(i):
+                    res.append((i, i.hostname))
+
+            return res
+
+        ping_actions = []
+        for i, hostname in (yield get_computes()):
+            action = PingCheckAction(i)
+            ping_actions.append((hostname, action.execute(DetachedProtocol(), object())))
+
+        self.log("Starting ping checks")
+        # wait for all async synchronization tasks to finish
+        for c, deferred in ping_actions:
+            try:
+                yield deferred
+            except Exception as e:
+                self.log("Got exception when pinging compute '%s': %s" % (c, e))
+                if get_config().getboolean('debug', 'print_exceptions'):
+                    traceback.print_exc()
+            else:
+                self.log("Pinging was ok for compute: '%s'" % c)
+
+        self.log("Ping check complete")
+
+provideSubscriptionAdapter(subscription_factory(PingCheckDaemonProcess), adapts=(Proc,))
 
 
 class SyncDaemonProcess(DaemonProcess):
