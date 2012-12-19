cea4fa36137c1ec828d5b880e173d0eeedb335cc
Andrei Sosnin <andrei.sosnin@gmail.com>
2012-12-19 23:42:21 +0200

    Implement ping using ping.do_one to better control its output (OMS-139)

--- opennode/knot/utils/icmp.py
+++ opennode/knot/utils/icmp.py
@@ -1,9 +1,11 @@
+import socket
 import subprocess
 
-from ping import quiet_ping
+from ping import do_one
+from twisted.python import log
 
 
-def ping(address):
+def ping(dest_addr, timeout=2, count=2, psize=64):
     """Pings a given host and returns True if it answers, otherwise False.
     It tries to use a pure python icmp library which requires begin root,
     so there is a `fping` fallback which is useful when running as a developer
@@ -11,10 +13,23 @@ def ping(address):
     """
 
     try:
-        lost, _, _ = quiet_ping(address, count=2)
+        plist = []
+        for i in xrange(count):
+            try:
+                delay = do_one(dest_addr, timeout, psize)
+            except socket.gaierror, e:
+                log.msg("ping %s failed. (socket error: '%s')" % (dest_addr, e[1]), system='ping')
+                break
+
+            if delay != None:
+                delay = delay * 1000
+                plist.append(delay)
+
+        # Find lost package percent
+        lost = 100 - (len(plist) * 100 / count)
         return lost != 100
     except:
         try:
-            return not subprocess.call('fping -q %s' % address, shell=True)
+            return not subprocess.call('fping -q %s' % dest_addr, shell=True)
         except:
             return True
