fcead549e93935a5d68204790942c4c4830a268d
Andrei Sosnin <andrei.sosnin@gmail.com>
2013-01-31 18:34:29 +0200

    Implement remote Salt execution support (OMS-192)

--- opennode/knot/backend/salt/compute.py
+++ opennode/knot/backend/salt/compute.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
+import subprocess
 
 from grokcore.component import context, subscribe
-
-import salt.config
-from salt.key import Key
+from twisted.internet import defer
+from twisted.python import log
 
 from opennode.knot.backend.compute import format_error
 from opennode.knot.model.machines import IIncomingMachineRequest
@@ -18,46 +18,53 @@ from opennode.oms.util import blocking_yield
 from opennode.oms.zodb import db
 
 
-class AcceptHostRequestAction(Action):
-    """Accept request of the host for joining OMS/Salt"""
+class BaseHostRequestAction(Action):
+    """Base host request action class"""
     context(IIncomingMachineRequest)
 
-    action('accept')
+    _action = None
+    _remote_option = None
 
     @db.ro_transact(proxy=False)
     def subject(self, *args, **kwargs):
         return tuple((self.context, ))
 
+    @defer.inlineCallbacks
     def execute(self, cmd, args):
-        try:
-            c_path = get_config().get('salt', 'master_config_path', '/etc/salt/master')
-            opts = salt.config.client_config(c_path)
-            key = Key(opts)
-            hostname = yield db.get(self.context, 'hostname')
-            yield key.accept(hostname)
-        except Exception as e:
-            cmd.write("%s\n" % format_error(e))
+        hostname = yield db.get(self.context, 'hostname')
+        remote_salt_key_cmd = get_config().get('salt', 'remote_key_command', None)
+        if remote_salt_key_cmd:
+            try:
+                output = subprocess.check_output([remote_salt_key_cmd, self._remote_option, hostname,
+                                                  '--no-color', '--out=raw'])
+                log.msg('Salt output: %s' % output, system='action-accept')
+            except subprocess.CalledProcessError as e:
+                cmd.write("%s\n" % format_error(e))
+        else:
+            try:
+                import salt.config
+                from salt.key import Key
+                c_path = get_config().get('salt', 'master_config_path', '/etc/salt/master')
+                opts = salt.config.client_config(c_path)
+                yield getattr(Key(opts), self._action)(hostname)
+            except Exception as e:
+                cmd.write("%s\n" % format_error(e))
 
 
-class RejectHostRequestAction(Action):
+
+class AcceptHostRequestAction(BaseHostRequestAction):
+    """Accept request of the host for joining OMS/Salt"""
+    action('accept')
+    _action = 'accept'
+    _remote_option = '-a'
+
+
+class RejectHostRequestAction(BaseHostRequestAction):
     """Remove request of the host for joining OMS/Salt"""
-    context(IIncomingMachineRequest)
-
     action('reject')
+    _action = 'reject'
+    _remote_option = '-r'
 
-    @db.ro_transact(proxy=False)
-    def subject(self, *args, **kwargs):
-        return self.context
-
-    def execute(self, cmd, args):
-        try:
-            c_path = get_config().get('salt', 'master_config_path', '/etc/salt/master')
-            opts = salt.config.client_config(c_path)
-            key = Key(opts)
-            hostname = yield db.get(self.context, 'hostname')
-            yield key.reject(hostname)
-        except Exception as e:
-            cmd.write("%s\n" % format_error(e))
 
 @subscribe(ICompute, IModelDeletedEvent)
 def delete_compute(model, event):
