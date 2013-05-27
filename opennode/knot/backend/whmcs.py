5d763009a36d12fe2ffe0a4db5dd5e08bceae55f
Ilja Livenson <ilja.livenson@gmail.com>
2013-05-28 00:38:34 +0300

    Fixed a bug with incorrect parsing of the empty WHMCS balance limit result

--- opennode/knot/backend/whmcs.py
+++ opennode/knot/backend/whmcs.py
@@ -100,7 +100,8 @@ class WhmcsCreditChecker(GlobalUtility):
             if data.get('result') == 'error':
                 raise WHMCSAPIError('%s' % (data.get('message')))
             credit_balance_field = 'customfields%s' % get_config().getstring('whmcs', 'balance_limit', 2)
-            credit_balance = float(data.get(credit_balance_field)) if credit_balance_field in data else 0
+            credit_balance = float(data.get(credit_balance_field)) if credit_balance_field in data \
+                                                and data.get(credit_balance_field) != "" else 0
             defer.returnValue((float(data.get('credit')), credit_balance))
 
         raise WHMCSAPIError('%s: %s' % (response.code, data))
