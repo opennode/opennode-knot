1c746a969e6fb3702621b35bcf644099bf3c738b
Andrei Sosnin <andrei.sosnin@gmail.com>
2013-07-15 14:20:11 +0300

    struct.unpack returns a tuple, need converting to list (OMS-524)

--- opennode/knot/utils/__init__.py
+++ opennode/knot/utils/__init__.py
@@ -2,6 +2,6 @@ import struct
 from os import urandom
 
 def mac_addr_kvm_generator():
-    bytelist = struct.unpack('BBB', urandom(3))
+    bytelist = list(struct.unpack('BBB', urandom(3)))
     bytelist[0] = bytelist[0] % 0x7f
     return '52:54:00:%02X:%02X:%02X' % (bytelist)
