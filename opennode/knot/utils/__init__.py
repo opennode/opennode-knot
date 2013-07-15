8be5102d7a7f8c748b20bd8cb9a5d10f7a4b1688
Andrei Sosnin <andrei.sosnin@gmail.com>
2013-07-15 14:31:42 +0300

    Add a test and fix issues with KVM MAC generator function (OMS-524)

--- opennode/knot/utils/__init__.py
+++ opennode/knot/utils/__init__.py
@@ -4,4 +4,4 @@ from os import urandom
 def mac_addr_kvm_generator():
     bytelist = list(struct.unpack('BBB', urandom(3)))
     bytelist[0] = bytelist[0] % 0x7f
-    return '52:54:00:%02X:%02X:%02X' % (bytelist)
+    return '52:54:00:%02X:%02X:%02X' % tuple(bytelist)
